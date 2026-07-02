a2f281e5014d5deead5fc7ec1f82fe970bf544d4
Horax <horaciogarciacr@gmail.com>
2026-07-01 21:00:39 -0300

    Nuevos trackeos

--- Grafico.py
+++ Grafico.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
 # 1. Cargar los datos del barrido recién adquirido
-archivo_csv = 'mapeo_scattering_punto_a_punto_v3.csv'
+version = 'v5_0107'
+
+archivo_csv = f'mapeo_scattering_punto_a_punto_{version}.csv'
 df = pd.read_csv(archivo_csv)
 
 # Extraemos las columnas de interés
@@ -36,7 +38,7 @@ y_micrones = y_discreto
 
 # 4. Crear una grilla regular (perfecta) para la imagen 2D
 # Definimos la resolución de los "píxeles" de nuestra imagen final
-num_pixels_x = 200 
+num_pixels_x = pasos_x_por_fila 
 num_pixels_y = cant_filas_y
 
 grid_x, grid_y = np.mgrid[0:8:complex(num_pixels_x), 0:y_micrones.max():complex(num_pixels_y)]
@@ -67,5 +69,5 @@ plt.ylabel('Posición Y ($\mu$m)')
 plt.grid(False)
 
 # Guardar la imagen del mapa
-plt.savefig('reconstruccion_muestra_2D.png', dpi=300)
+plt.savefig(f'reconstruccion_muestra_2D_{version}.png', dpi=300)
 plt.show()
